fca5011eea4fec82fdf2e65b3738ce7db919d367
Abin Gadoo <abin.gadoo@in.ibm.com>
2021-03-09 15:45:09 +0530

    Added new button for a new stack

--- app/src/main/java/com/test/vinambek/crashworld/MainActivity.java
+++ app/src/main/java/com/test/vinambek/crashworld/MainActivity.java
@@ -8,12 +8,19 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    Button crashButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        crashButton = findViewById(R.id.crashMe);
+
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                throw new RuntimeException("Intentional Test Crash for Crashlytics"); // Force a crash
+            }
+        });
     }
 
     public void onClick(View view)
